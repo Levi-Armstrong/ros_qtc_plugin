8204513892839878192187b97852c688b011eb2d
Levi Armstrong <levi.armstrong@gmail.com>
2019-04-30 20:33:42 -0500

    Update latest installer to 0.3.3

--- installer/create_latest_installer.sh
+++ installer/create_latest_installer.sh
@@ -9,22 +9,22 @@ DIR_PATH=`dirname $FILE_PATH`
 source "$DIR_PATH/installer_functions.sh"
 
 # Create Installer data for latest versions
-INSTALLER_VERSION=1.0.4 # This must be incremented for every new release
+INSTALLER_VERSION=1.0.5 # This must be incremented for every new release
 INSTALLER_RELEASE_DATE=2019-02-03
 
 QTC_LATEST=1
 QTC_REPO="https://github.com/qt-creator/qt-creator.git"
-QTC_VERSION=4.8.1.0
-QTC_BRANCH=v4.8.1
-QTC_RELEASE_DATE=2018-10-23
-RQTC_VERSION=0.3.2.0
-RQTC_RELEASE_DATE=2019-02-03
+QTC_VERSION=4.8.2.0
+QTC_BRANCH=v4.8.2
+QTC_RELEASE_DATE=2019-03-01
+RQTC_VERSION=0.3.3.0
+RQTC_RELEASE_DATE=2019-04-22
 QTERMWIDGET_BRANCH="debian/xenial"
 SortingPriority=200
 
 CLANG_FILENAME=libclang-release_60-linux-Rhel7.2-gcc5.3-x86_64-clazy.7z # Name must be located here https://download.qt.io/development_releases/prebuilt/libclang/
 YAML_CPP_TAG=yaml-cpp-0.6.2
-QT_PATH=/home/Qt5.11.2/5.11.2/gcc_64 # This must be the same version used for qtcreator.7z and qtcreator_dev.7z
+QT_PATH=/home/Qt5.12.3/5.12.3/gcc_64 # This must be the same version used for qtcreator.7z and qtcreator_dev.7z
 QIFW_PATH=/home/QtIFW-3.0.2/bin/binarycreator
 
 createInstaller
